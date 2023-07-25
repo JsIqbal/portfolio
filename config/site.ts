d7c33df66d2bb109cfee9be321eeb664cda0e8d1
jsKraken <javascriptiqbal@gmail.com>
2023-07-25 17:15:34 +0600

    Bug

--- config/site.ts
+++ config/site.ts
@@ -8,7 +8,7 @@ export const siteConfig = {
     navItems: [
         {
             label: "Home",
-            href: `/next-portfolio`,
+            href: `/`,
         },
         {
             label: "Docs",
