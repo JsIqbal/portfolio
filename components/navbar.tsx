a4770bb14990b384c4987fb4a6685814ffdb16cf
jsKraken <javascriptiqbal@gmail.com>
2023-07-25 23:12:13 +0600

    Fix

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -31,27 +31,6 @@ import {
 import { Logo } from "@/components/icons";
 
 export const Navbar = () => {
-    const searchInput = (
-        <Input
-            aria-label="Search"
-            classNames={{
-                inputWrapper: "bg-default-100",
-                input: "text-sm",
-            }}
-            endContent={
-                <Kbd className="hidden lg:inline-block" keys={["command"]}>
-                    K
-                </Kbd>
-            }
-            labelPlacement="outside"
-            placeholder="Search..."
-            startContent={
-                <SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
-            }
-            type="search"
-        />
-    );
-
     return (
         <NextUINavbar maxWidth="2xl" position="sticky">
             <NavbarContent
@@ -120,9 +99,7 @@ export const Navbar = () => {
                     </Link>
                     <ThemeSwitch />
                 </NavbarItem>
-                <NavbarItem className="hidden lg:flex">
-                    {searchInput}
-                </NavbarItem>
+
                 <NavbarItem className="hidden md:flex">
                     <Button
                         isExternal
@@ -152,7 +129,6 @@ export const Navbar = () => {
             </NavbarContent>
 
             <NavbarMenu>
-                {searchInput}
                 <div className="mx-4 mt-2 flex flex-col gap-2">
                     {siteConfig.navMenuItems.map((item, index) => (
                         <NavbarMenuItem key={`${item}-${index}`}>
