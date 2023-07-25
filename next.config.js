9e43cc3916dbbc8bab7be686d7d7d0a76f1c8a65
jsKraken <javascriptiqbal@gmail.com>
2023-07-26 01:41:18 +0600

    Feature/UI

--- next.config.js
+++ next.config.js
@@ -1,17 +1,17 @@
-module.exports = {
-    basePath: "/portfolio", // Replace 'your-repo-name' with your GitHub repository name
-    assetPrefix: "/portfolio/", // Replace 'your-repo-name' with your GitHub repository name
-    output: "export", // Enable static HTML export
+// module.exports = {
+//     basePath: "/portfolio", // Replace 'your-repo-name' with your GitHub repository name
+//     assetPrefix: "/portfolio/", // Replace 'your-repo-name' with your GitHub repository name
+//     output: "export", // Enable static HTML export
 
-    // Add other configurations if needed
-    homepage: "https://jsiqbal.github.io/portfolio",
-};
-
-// /**
-//  * @type {import('next').NextConfig}
-//  */
-// const nextConfig = {
-//     /* config options here */
+//     // Add other configurations if needed
+//     homepage: "https://jsiqbal.github.io/portfolio",
 // };
 
-// module.exports = nextConfig;
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+    /* config options here */
+};
+
+module.exports = nextConfig;
