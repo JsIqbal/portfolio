9267732457e3f93683ae6e27a62d6c84fba57d8f
jsKraken <javascriptiqbal@gmail.com>
2023-07-27 13:55:26 +0600

    Update Fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,6 @@ import { Navbar } from "@/components/navbar";
 import { Link } from "@nextui-org/link";
 import clsx from "clsx";
 import GridDefault from "@/components/grid-default";
-import { config } from "dotenv";
-
-config();
 
 export const metadata: Metadata = {
     title: {
