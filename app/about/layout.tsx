c3b3472f1c3afe407b81275e0f1bc973cf55ed0e
jsKraken <javascriptiqbal@gmail.com>
2023-07-31 00:41:18 +0600

    Add About Me UI

--- app/about/layout.tsx
+++ app/about/layout.tsx
@@ -5,7 +5,7 @@ export default function AboutLayout({
 }) {
     return (
         <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-            <div className="inline-block max-w-xxl px-12 text-center justify-center">
+            <div className="inline-block max-w-xxl px-8 text-center justify-center">
                 {children}
             </div>
         </section>
