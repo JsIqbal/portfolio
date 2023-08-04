11721f3daf9263b886a38372a459420075f32d05
jsKraken <javascriptiqbal@gmail.com>
2023-08-04 16:25:25 +0600

    Work With Responsive Nav

--- config/site.ts
+++ config/site.ts
@@ -25,36 +25,20 @@ export const siteConfig = {
     ],
     navMenuItems: [
         {
-            label: "Profile",
-            href: "/profile",
+            label: "Home",
+            href: `/`,
         },
         {
-            label: "Dashboard",
-            href: "/dashboard",
+            label: "Docs",
+            href: "/docs",
         },
         {
-            label: "Projects",
-            href: "/projects",
+            label: "Pricing",
+            href: "/pricing",
         },
         {
-            label: "Team",
-            href: "/team",
-        },
-        {
-            label: "Calendar",
-            href: "/calendar",
-        },
-        {
-            label: "Settings",
-            href: "/settings",
-        },
-        {
-            label: "Help & Feedback",
-            href: "/help-feedback",
-        },
-        {
-            label: "Logout",
-            href: "/logout",
+            label: "Blog",
+            href: "/blog",
         },
     ],
     links: {
