0e22236dcd31d3f01230856d7a62f0116945efa7
jsKraken <javascriptiqbal@gmail.com>
2023-08-04 15:07:18 +0600

    Update About Me Page For Home

--- config/site.ts
+++ config/site.ts
@@ -22,10 +22,6 @@ export const siteConfig = {
             label: "Blog",
             href: "/blog",
         },
-        {
-            label: "About",
-            href: "/about",
-        },
     ],
     navMenuItems: [
         {
